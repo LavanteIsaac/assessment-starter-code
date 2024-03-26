465f9a90125efc176309d9d81d3f066129293cf8
Lavante Cotton <lavanteisaac@gmail.com>
2024-03-26 15:29:59 -0400

    get delete

--- server/app.py
+++ server/app.py
@@ -1,7 +1,23 @@
-from flask import request, make_response
+from flask import request, make_response, jsonify
+
 from config import app, db, api
 from models import Project
 from flask_restful import Resource
 
 import ipdb
 
+@app.route('/projects', mehtods=['GET'])
+def all_projects():
+    projects = Project.query.all()
+    projects = {project.to_dict() for project in projects}
+    return jsonfiy(projects_list), 200
+
+@app.route('/projects/<int:id>', methods= ['DELETE'])
+def post_by_id(id):
+    if request.method == 'DELETE':
+        db.sessions.delete(project)
+        db.sessions.commit()
+
+        return make_response(' ', 204)
+    elif project is None:
+        return make_reponse(jsonfiy({'error': 'Project not found'}), 404)
