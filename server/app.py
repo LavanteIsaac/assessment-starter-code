397f7a512ff89b76df94d46ca0c89732b25b2b5c
Lavante Cotton <lavanteisaac@gmail.com>
2024-03-27 12:43:07 -0400

    typos in get and fixed query moved error message

--- server/app.py
+++ server/app.py
@@ -9,18 +9,21 @@ import ipdb
 @app.route('/projects', methods= ["GET"])
 def all_projects():
     projects= Project.query.all()
-    projects_list = [project.to_dict() for projects in projects]
+    projects_list = [project.to_dict() for project in projects]
      
-    return make_response(project_list), 200
+    return make_response(projects_list), 200
 
 @app.route('projects/<int:id>', methods= ['DELETE'])
 def project_by_id(id):
-    project = Project.query.all()
-    if request.method == 'DELETE':
+    project = Project.query.get(id)
+    if project is None:
+        return make_response({'error:' 'No project'})
+
+    elif request.method == 'DELETE':
         db.session.delete(project)
         db.session.commit()
 
         return make_response('', 204)
-    elif project is None:
-        return make_response({'error:' 'No project'})
+    
+        
     
